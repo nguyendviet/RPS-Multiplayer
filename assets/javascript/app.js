aa20c57444808af2f0d98660cbcfe7ed9928cd7e
Viet Nguyen <vietjul@gmail.com>
2017-09-13 10:36:28 -0400

    basic html, no logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	//print input and submit player name button
+
+	//print 2 box for 2 players
+	//print middle box
+	//players are objects: contain RPS, win, lose
+
+	//1st user enters name becomes 1st player
+	//2nd user enters becomes 2nd player (firebase?)
+
+	//users choose R/P/S, users can see what they chose, but the other can't
+	//both players see whose turn it is
+
+	//compare RPS and decides who wins point
+	//reset, play another round
+
+	//players can leave anytime, free space for someone else to join
+
+	//chat box:
+	//board shows all messages sent
+	//bar type new message and send button
+});
